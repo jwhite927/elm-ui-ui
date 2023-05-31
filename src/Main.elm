799d6330b0a475fead2a518a545d1bb57d079fa5
Josh White <jwhite927@gmail.com>
2023-05-31 10:38:17 -0400

    Add node selection

--- src/Main.elm
+++ src/Main.elm
@@ -4,9 +4,11 @@ import Browser
 import Element as El
 import Element.Background as Background
 import Element.Border as Border
+import Element.Events as Events
 import Element.Font as Font
 import Element.Input as Input
 import Html exposing (Html)
+import String.Interpolate as SI
 
 
 main : Program () Model Msg
@@ -21,19 +23,21 @@ main =
 
 type alias Model =
     { tree : Node
-    , selected : Node
+    , selected : Id
+    , nextId : Int
     }
 
 
+init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { tree = Text "Okay", selected = None }, Cmd.none )
+    ( { tree = Text 0 "Okay", selected = -1, nextId = 1 }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
     let
         ( ui, code ) =
-            parse model.tree 0
+            parse model.tree 0 model.selected
     in
     El.layout
         [ El.width El.fill
@@ -45,7 +49,13 @@ view model =
             , El.spacing 5
             , El.height El.fill
             ]
-            [ El.column ([ El.width El.fill, El.height El.fill, El.spacing 10 ] ++ border)
+            [ El.column
+                ([ El.width El.fill
+                 , El.height El.fill
+                 , El.spacing 10
+                 ]
+                    ++ border
+                )
                 [ El.el [ El.centerX, El.padding 5 ] <| El.text "Code"
                 , El.el
                     [ Background.color <| El.rgb255 0 0 0
@@ -56,8 +66,8 @@ view model =
                     , Border.rounded 5
                     ]
                   <|
-                    El.text code
-                , Input.button [ El.centerX]
+                    code
+                , Input.button [ El.centerX ]
                     { label =
                         El.el
                             ([ El.width El.fill, El.padding 5 ] ++ border)
@@ -65,7 +75,7 @@ view model =
                             El.text "Add Text"
                     , onPress = Just AddText
                     }
-                , Input.button [ El.centerX]
+                , Input.button [ El.centerX ]
                     { label =
                         El.el
                             ([ El.width El.fill, El.padding 5 ] ++ border)
@@ -73,6 +83,7 @@ view model =
                             El.text "Add Element"
                     , onPress = Just AddElement
                     }
+                , selectedMenu model
                 ]
             , El.column
                 ([ El.width El.fill
@@ -90,61 +101,155 @@ type Msg
     = NoOp
     | AddText
     | AddElement
+    | Selected Id
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
+    let
+        nextNextId =
+            model.nextId + 1
+    in
     case msg of
         NoOp ->
             ( model, Cmd.none )
 
         AddText ->
-            ( { model | tree = Text "Some text" }, Cmd.none )
+            ( { model
+                | tree = Text model.nextId "Some text"
+                , nextId = nextNextId
+              }
+            , Cmd.none
+            )
 
         AddElement ->
-            ( { model | tree = El model.tree }, Cmd.none )
+            ( { model
+                | tree = El model.nextId model.tree
+                , nextId = nextNextId
+              }
+            , Cmd.none
+            )
+
+        Selected id ->
+            ( { model | selected = id }, Cmd.none )
 
 
+border : List (El.Attribute msg)
 border =
-    [ Border.width 1, Border.color <| El.rgb255 0 0 0, Border.rounded 5 ]
+    [ Border.width 1
+    , Border.color <| El.rgb255 0 0 0
+    , Border.rounded 5
+    ]
 
 
-parse : Node -> Int -> ( El.Element msg, String )
-parse tree indent =
+parse : Node -> Int -> Id -> ( El.Element msg, El.Element Msg )
+parse tree indent selected =
     let
+        tabs x =
+            List.repeat x "  " |> String.join ""
+
         pipe =
             if indent == 0 then
                 ""
 
             else
-                "<| "
+                tabs (indent - 1) ++ "<|\n"
+
+        selectedStyle id =
+            if id == selected then
+                [ Font.color <| El.rgb255 0 0 255 ]
+
+            else
+                [ Font.color <| El.rgb255 255 255 255 ]
     in
     case tree of
-        Text value ->
+        Text id value ->
             ( El.text value
-            , pipe
-                ++ "El.text \""
-                ++ value
-                ++ "\""
+            , El.el ((Events.onClick <| Selected id) :: selectedStyle id) <|
+                El.text <|
+                    SI.interpolate
+                        "{1}{0}El.text \"{2}\""
+                        [ tabs indent, pipe, value ]
             )
 
-        El node ->
+        El id node ->
             let
                 ( child, code ) =
-                    parse node (indent + 1)
+                    parse node (indent + 1) selected
             in
             ( El.el
                 [ El.explain Debug.todo ]
                 child
-            , pipe ++ "El.el\n\t[]\n\t" ++ code ++ "\""
+            , El.column [] <|
+                [ El.el ((Events.onClick <| Selected id) :: selectedStyle id) <|
+                    El.text <|
+                        SI.interpolate
+                            "{1}{0}El.el\n{2}[]{0}"
+                            [ tabs indent, pipe, tabs (indent + 1) ]
+                , code
+                ]
             )
 
         _ ->
-            ( El.none, "" )
+            ( El.none, El.none )
 
 
 type Node
-    = El Node
-    | Text String
-    | Row (List Node)
-    | Column (List Node)
+    = El Id Node
+    | Text Id String
+    | Row Id (List Node)
+    | Column Id (List Node)
     | None
+
+
+type alias Id =
+    Int
+
+
+getId : Node -> Id
+getId node =
+    case node of
+        El id _ ->
+            id
+
+        Text id _ ->
+            id
+
+        Row id _ ->
+            id
+
+        Column id _ ->
+            id
+
+        None ->
+            -1
+
+
+selectedMenu model =
+    let
+        node =
+            findNodeById model.tree model.selected
+    in
+    case node of
+        El _ child ->
+            El.text "Element selected"
+
+        Text _ value ->
+            El.text <| "Text with value " ++ value ++ " selected"
+
+        _ ->
+            El.text "None selected"
+
+
+findNodeById : Node -> Id -> Node
+findNodeById tree id =
+    if getId tree == id then
+        tree
+
+    else
+        case tree of
+            El _ child ->
+                findNodeById child id
+
+            _ ->
+                None
